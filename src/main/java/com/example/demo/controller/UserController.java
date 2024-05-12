f93ade14124375fca4204398467c7bb75e535f38
dongphamhoang <hoangphamdong.it@gmail.com>
2024-05-12 08:31:55 +0700

    commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.example.demo.domain.User;
+import com.example.demo.service.UserService;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.helloService();
+        model.addAttribute("test", test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("run here: " + user);
+        // model.addAttribute("newUser", new User());
+        return "hello";
+    }
+}
