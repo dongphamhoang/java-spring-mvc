1a8b16c18a5a17069ba7efe21256863e2ce7f17d
dongphamhoang <hoangphamdong.it@gmail.com>
2024-05-19 13:34:09 +0700

    domain

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -7,13 +9,16 @@ import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -26,16 +31,49 @@ public class UserController {
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
-    public String getUserPage(Model model) {
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
-        System.out.println("run here: " + user);
-        // model.addAttribute("newUser", new User());
-        return "hello";
+        this.userService.handleSaveUser(user);
+        return "redirect:/admin/user";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/{userId}")
+    public String getUserDetailPage(Model model, @PathVariable long userId) {
+        User user = this.userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "admin/user/show";
+    }
+
+    @RequestMapping(value = "/admin/user/update/{userId}")
+    public String updateUserPage(Model model, @PathVariable long userId) {
+        User user = this.userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "admin/user/update";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String updateUser(Model model, @ModelAttribute("user") User user) {
+        User currentUser = this.userService.getUserById(user.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(user.getAddress());
+            currentUser.setFullName(user.getFullName());
+            currentUser.setPhone(user.getPhone());
+            this.userService.handleSaveUser(currentUser);
+        }
+        return "redirect:/admin/user";
+    }
+
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,33 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public String helloService() {
         return "hello service";
+    }
+
+    public void handleSaveUser(User user) {
+        this.userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User getUserById(long userId) {
+        return this.userRepository.findById(userId);
     }
 }
