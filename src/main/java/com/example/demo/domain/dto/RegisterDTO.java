566499caab2eb17e2e26ac39ef5c423fb1d9e1a2
dongphamhoang <hoangphamdong.it@gmail.com>
2024-06-02 12:26:01 +0700

    commit

--- src/main/java/com/example/demo/domain/dto/RegisterDTO.java
+++ src/main/java/com/example/demo/domain/dto/RegisterDTO.java
@@ -2,12 +2,26 @@ package com.example.demo.domain.dto;
 
 import com.example.demo.service.validator.RegisterChecked;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @RegisterChecked
 public class RegisterDTO {
+    @Size(min = 3, message = "First Name Phải có tối thiểu 3 ký tự")
     private String firstName;
     private String lastName;
+
+    @NotNull
+    @Email(message = "Email không hợp lệ", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
+
+    @NotNull
     private String password;
+
+    @Size(min = 3, message = "Confirm Password Phải có tối thiểu 3 ký tự")
     private String confirmPassword;
 
     public String getFirstName() {
