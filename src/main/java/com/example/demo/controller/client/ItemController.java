c6010f35696fccd0673f700fe0e3be9d6d509a41
dongphamhoang <hoangphamdong.it@gmail.com>
2024-06-08 18:01:36 +0700

    commit

--- src/main/java/com/example/demo/controller/client/ItemController.java
+++ src/main/java/com/example/demo/controller/client/ItemController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.domain.Cart;
+import com.example.demo.domain.CartDetail;
 import com.example.demo.domain.Product;
+import com.example.demo.domain.User;
 import com.example.demo.service.ProductService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -41,7 +46,24 @@ public class ItemController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage(Model model) {
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();// null
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+
+        List<CartDetail> cartDetails = cart.getCartDetails();
+
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
+
         return "client/cart/show";
     }
 
